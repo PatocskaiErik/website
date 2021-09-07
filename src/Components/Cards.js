57f2cb2598d44e3f89266f2dc09289fba4dd16b0
Erik <patoerik93@gmail.com>
2021-09-07 18:47:47 +0200

    cards

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,19 +1,45 @@
 import React from 'react';
-import CardItem from './CardItem';
 import './Cards.css';
+import CardItem from './CardItem';
 
 const Cards = () => {
   return (
     <div className='cards'>
-      <h1>Check out these characters!</h1>
-      <div className='cards_container'>
-        <div className='cards_wrapper'>
-          <ul className='cards_items'>
+      <h1>Check out these EPIC Characters!</h1>
+      <div className='cards__container'>
+        <div className='cards__wrapper'>
+          <ul className='cards__items'>
             <CardItem
-              src='images/img-9.jpg'
-              text='valami'
-              label='label'
+              src='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/mh-mandalorian-characters-lead-1572974175.png?crop=0.470xw:0.939xh;0,0&resize=640:*'
+              text='Mandalorian'
+              label='Adventure'
               path='/services'
+            />
+            <CardItem
+              src='https://static.highsnobiety.com/thumbor/w4w665qE1T9INU6V_G8XCp5rdHU=/1600x1067/static.highsnobiety.com/wp-content/uploads/2019/12/01152437/baby-yoda-sipping-soup-best-reactions-01.jpg'
+              text='Baby Yoda'
+              label='Baby'
+              path='/services'
+            />
+          </ul>
+          <ul className='cards__items'>
+            <CardItem
+              src='https://www.looper.com/img/gallery/mandalorian-season-2-characters-who-mean-more-than-you-know/l-intro-1608312558.jpg'
+              text='Jedi Character'
+              label='Fight'
+              path='/services'
+            />
+            <CardItem
+              src='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/mandalorian-season-3-1608310180.jpg'
+              text='Family'
+              label='Mistery'
+              path='/products'
+            />
+            <CardItem
+              src='https://static2.srcdn.com/wordpress/wp-content/uploads/2021/04/The-Mandalorian-season-3-dark-saber-baby-yoda-bo-Katan.jpg'
+              text='Jedi characters'
+              label='Adrenaline'
+              path='/sign-up'
             />
           </ul>
         </div>
@@ -21,4 +47,5 @@ const Cards = () => {
     </div>
   );
 };
+
 export default Cards;
